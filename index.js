588f46796f707ff7b0fb5aa045c25a8aa2161246
Ottology <soccer14otto@gmail.com>
2022-08-04 19:43:12 -0700

    step 2

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let weeklyExpenses = 1
+let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+console.log(answer)
